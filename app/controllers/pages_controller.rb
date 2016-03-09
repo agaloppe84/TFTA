488b25641c18cc79eb67e1dff078abd114a1a36a
martin malinvaud <martin.malinvaud@gmail.com>
2016-03-09 19:38:35 +0100

    en cours

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,18 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
+
   def home
     @foodtrucks = Foodtruck.all
   end
 
   def profil
     if current_user.foodtrucker?
-     @foodtruck = current_user.foodtrucks.first
-   end
+      render :profil_foodtrucker
+    else
+      render :profil_user
+    end
   end
+
 
 end
