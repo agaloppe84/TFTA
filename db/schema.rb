bf31c5c48651b26d8bdd8b1822cc602b51ba73cd
martin malinvaud <martin.malinvaud@gmail.com>
2016-03-10 10:31:08 +0100

    marche pas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160309140843) do
+ActiveRecord::Schema.define(version: 20160310091948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20160309140843) do
     t.integer  "culinary_style_id"
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
+    t.string   "photo"
   end
 
   add_index "foodtrucks", ["culinary_style_id"], name: "index_foodtrucks_on_culinary_style_id", using: :btree
